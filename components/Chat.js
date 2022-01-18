d0e284839665829594260270e12f791070796616
gbstrom <85531363+gbstrom@users.noreply.github.com>
2022-01-18 10:25:53 -0500

    created static message to see how things display

--- components/Chat.js
+++ components/Chat.js
@@ -9,6 +9,24 @@ export class Chat extends React.Component {
       messages: [],
     }
   }
+
+  componentDidMount() {
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: 'Hello developer',
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: 'React Native',
+            avatar: 'https://placeimg.com/140/140/any',
+          },
+        },
+      ],
+    })
+  }
+  
   render() {
     let name = this.props.route.params.name; // OR ...
     // let { name } = this.props.route.params;
