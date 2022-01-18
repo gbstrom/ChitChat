d4672cd7935811f141b1702b9dc3566c1a81a324
gbstrom <85531363+gbstrom@users.noreply.github.com>
2022-01-18 10:22:08 -0500

    added state initialization in Chat.js contructor

--- components/Chat.js
+++ components/Chat.js
@@ -2,8 +2,13 @@ import React from 'react';
 import { View, Text } from 'react-native';
 import { GiftedChat } from 'react-native-gifted-chat'
 
-export default class Chat extends React.Component {
-
+export class Chat extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      messages: [],
+    }
+  }
   render() {
     let name = this.props.route.params.name; // OR ...
     // let { name } = this.props.route.params;
