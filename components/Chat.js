a71342c2707792f5992c3910c441a0a874645818
gbstrom <85531363+gbstrom@users.noreply.github.com>
2022-01-19 11:56:20 -0500

    tidied up the code a bit and added some comments

--- components/Chat.js
+++ components/Chat.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View, Platform, KeyboardAvoidingView } from 'react-native';
-import { GiftedChat } from 'react-native-gifted-chat'
+import { GiftedChat, Bubble } from 'react-native-gifted-chat'
 
 export default class Chat extends React.Component {
   constructor() {
@@ -15,7 +15,7 @@ export default class Chat extends React.Component {
       messages: [
         {
           _id: 1,
-          text: 'Hello developer',
+          text: 'Hello developer. It is time to converse.',
           createdAt: new Date(),
           user: {
             _id: 2,
@@ -23,9 +23,12 @@ export default class Chat extends React.Component {
             avatar: 'https://placeimg.com/140/140/any',
           },
         },
+        // This next bit is the code for the system message that welcomes the user by name when they start chatting.
+        // Note that I used this.props.route.params.name even though the instructions said to use this.props.navigation.state.params.name;
+        // I couldn't get that to work.
         {
          _id: 2,
-         text: 'This is a system message',
+         text: 'Hello ' + this.props.route.params.name + '. Welcome to the chat!',
          createdAt: new Date(),
          system: true,
         },
@@ -39,6 +42,21 @@ export default class Chat extends React.Component {
     }));
   }
 
+
+  // This is the function that makes the background color of the sender's text bubbles black instead of blue.
+  renderBubble(props) {
+    return (
+      <Bubble
+        {...props}
+        wrapperStyle={{
+          right: {
+            backgroundColor: '#000'
+          }
+        }}
+      />
+    )
+  }
+
   render() {
     let name = this.props.route.params.name; // OR ...
     // let { name } = this.props.route.params;
@@ -50,12 +68,14 @@ export default class Chat extends React.Component {
     return (
       <View style={{flex:1}}>
         <GiftedChat
+          renderBubble={this.renderBubble.bind(this)}
           messages={this.state.messages}
           onSend={messages => this.onSend(messages)}
           user={{
             _id: 1,
           }}
         />
+        {/* This code here is necessary to prevent the keyboard from blocking the input box on older Android phones. */}
         { Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null }
       </View>
        /* <View style={{
