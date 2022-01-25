19507d6be4a30f7e85fb39cb35e3586bdd1f95c6
gbstrom <85531363+gbstrom@users.noreply.github.com>
2022-01-25 10:47:00 -0500

    got the code to send messages to the FB collection

--- components/Chat.js
+++ components/Chat.js
@@ -20,6 +20,7 @@ export default class Chat extends React.Component {
     super();
     this.state = {
       messages: [],
+			uid: 0,
       user: {
 				_id: '',
 				name: '',
@@ -36,54 +37,81 @@ export default class Chat extends React.Component {
   }
 
   componentDidMount() {
-    this.setState({
-      messages: [
-        {
-          _id: 1,
-          text: 'Hello developer',
-          createdAt: new Date(),
-          user: {
-            _id: 2,
-            name: 'React Native',
-            avatar: 'https://placeimg.com/140/140/any',
-          },
-        },
-        // This next bit is the code for the system message that welcomes the user by name when they start chatting.
-        // Note that I used this.props.route.params.name even though the instructions said to use this.props.navigation.state.params.name;
-        // I couldn't get that to work.
-        {
-         _id: 2,
-         text: 'Hello ' + this.props.route.params.name + '. Welcome to the chat!',
-         createdAt: new Date(),
-         system: true,
-        },
-      ],
-    })
+    // let name = this.props.route.params.name; // OR ...
+    let { name } = this.props.route.params;
 
+    this.props.navigation.setOptions({ title: name });
 
     this.referenceChatMessages = firebase.firestore().collection("messages");
-    this.unsubscribe = this.referenceChatMessages.onSnapshot(this.onCollectionUpdate)
+    this.authUnsubscribe = firebase.auth().onAuthStateChanged(async (user) => {
+      if (!user) {
+        return await firebase.auth().signInAnonymously();
+      }
+      this.setState({
+        uid: user.uid,
+        messages: [],
+        user: {
+          _id: user.uid,
+          name: name,
+          avatar: "https://placeimg.com/140/140/any",
+        },
+      });
+      this.unsubscribe = this.referenceChatMessages
+        .orderBy("createdAt", "desc")
+        .onSnapshot(this.onCollectionUpdate);
+    });
 
-  }
+    //     // This next bit is the code for the system message that welcomes the user by name when they start chatting. I abolished it because
+    //     // it seemed no longer to be required after 5.2, but I've kept it as commented code here in case it's needed later.
+    //     // Note that I used this.props.route.params.name even though the instructions said to use this.props.navigation.state.params.name;
+    //     // I couldn't get that to work.
+    //     {
+    //      _id: 2,
+    //      text: 'Hello ' + this.props.route.params.name + '. Welcome to the chat!',
+    //      createdAt: new Date(),
+    //      system: true,
+    //     },
 
+  };
+
+  componentWillUnmount() {
+    this.unsubscribe();
+    this.authUnsubscribe();
+ }
+
+  // this function adds whatever the user has just typed in as a new document in the firebase collection. It is called inside the onSend function.
   addMessage() {
 		const message = this.state.messages[0];
+    // console.log(this.state.messages);
 		// add a new message to the collection
+    // console.log(message._id,)
 		this.referenceChatMessages.add({
 			_id: message._id,
-			text: message.text || '',
+			text: message.text,
 			createdAt: message.createdAt,
 			user: this.state.user,
+      uid: this.state.uid,
 		});
-	}
+	};
+
+  // this is the function that is supposed to be called when someone presses the send button, which is set up in the GiftedChat component.
+  // The function takes whatever the user has typed in, and does two things with it:
+  // 1. it adds it as a new message to the state of the app instance running on the user's device;
+  // 2. it runs addMessage(), which causes the user's new message to be added as a new document in the firebase collection.
 
   onSend(messages = []) {
-    this.setState((previousState) => ({
+    this.setState(previousState => ({
       messages: GiftedChat.append(previousState.messages, messages),
-    }));
+    }), () => {
     this.addMessage();
-  }
+  });
 
+// the problem with my onSend code is that addMessage is happening before the setState code is done because it should be asynchronous.
+
+//  this.setState({ title: event.target.value }, () => this.APICallFunction());
+
+  // this function gets all the messages in firebase and adds them to the state of the app instance running on the user's device. It is constantly
+  // running in somme mysterious way that is set up in the context of the unsubscribe function in componentDidMount.
   onCollectionUpdate = (querySnapshot) => {
     const messages = [];
     // go through each document
@@ -97,7 +125,12 @@ export default class Chat extends React.Component {
         user: data.user,
       });
     });
+    this.setState({
+      messages
+    });
+    // console.log(this.state.messages);
   };
+}
 
   // This is the function that makes the background color of the sender's text bubbles black instead of blue.
   renderBubble(props) {
@@ -111,14 +144,9 @@ export default class Chat extends React.Component {
         }}
       />
     )
-  }
+  };
 
   render() {
-    let name = this.props.route.params.name; // OR ...
-    // let { name } = this.props.route.params;
-
-    this.props.navigation.setOptions({ title: name });
-
     const backgroundColor = this.props.route.params.backgroundColor;
 
     return (
@@ -126,9 +154,11 @@ export default class Chat extends React.Component {
         <GiftedChat
           renderBubble={this.renderBubble.bind(this)}
           messages={this.state.messages}
-          onSend={messages => this.onSend(messages)}
+          onSend={usersNewMessage => this.onSend(usersNewMessage)}
           user={{
-            _id: 1,
+            _id: this.state.user._id,
+            name: this.state.name,
+            avatar: this.state.user.avatar,
           }}
         />
         {/* This code here is necessary to prevent the keyboard from blocking the input box on older Android phones. */}
