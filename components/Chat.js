0772b4a3ac8463a8bcd6fdec2b37663ebad81db9
gbstrom <85531363+gbstrom@users.noreply.github.com>
2022-01-27 13:40:22 -0500

    imported Image from react-native

--- components/Chat.js
+++ components/Chat.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Platform, KeyboardAvoidingView } from 'react-native';
+import { View, Platform, KeyboardAvoidingView, Image } from 'react-native';
 import { GiftedChat, Bubble, InputToolbar } from 'react-native-gifted-chat';
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
